304c81edcb297ede9812096a192703d3d7d83353
AlexVillasenor1 <isaac.villasenor2923@alumnos.udg.mx>
2020-09-29 17:06:23 +0000

    Calculo de numero e con ciclos

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var n float64
+	var e float64 = 0
+	fmt.Scan(&n)
+	for i:=0.0; i<n; i++{
+		fact := 1.0
+		for j:=1.0; j<=i; j++{
+			fact *= j
+		}
+		e += 1.0 /fact
+	}
+
+	fmt.Print(e)
 }
