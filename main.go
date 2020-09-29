bf4001aa4cb73ed67b7fa2061d8d40d983830e28
AlexVillasenor1 <isaac.villasenor2923@alumnos.udg.mx>
2020-09-29 17:17:24 +0000

    Se toma en cuenta el limite n

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func main()  {
+func main(){
 	var n float64
 	var e float64 = 0
 	fmt.Scan(&n)
-	for i:=0.0; i<n; i++{
+	for i:=0.0; i<=n; i++{
 		fact := 1.0
 		for j:=1.0; j<=i; j++{
 			fact *= j
